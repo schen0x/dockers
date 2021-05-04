8b56ffd0aa32370a48ee490af811214934f1eaf2
schen0x <schen.git@gmail.com>
2021-05-05 02:39:11 +0900

    mod config

--- dont-starve-server/cluster-content/Master/modoverrides.lua
+++ dont-starve-server/cluster-content/Master/modoverrides.lua
@@ -2,5 +2,15 @@ return {
 --#Global Position
 ["workshop-378160973"] = { enabled = true },
 --#Health Info
-["workshop-375859599"] = { enabled = true }
+--["workshop-375859599"] = { enabled = true },
+--#Simple Health Bar
+["workshop-1207269058"] = { enabled = true },
+--#Cluster Plant. Reborn!
+["workshop-1337321458"] = { enabled = true },
+--#PartyHUD
+["workshop-782961570"] = { enabled = true },
+--#No More Respawn Penalty
+["workshop-356420397"] = { enabled = true },
+--#Campfire Respawn
+["workshop-569043634"] = { enabled = true }
 }
--- dont-starve-server/run_dedicated_servers.sh
+++ dont-starve-server/run_dedicated_servers.sh
@@ -28,8 +28,31 @@ check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
 # ./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
-./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 +quit
-echo $'ServerModSetup("378160973")\nServerModSetup("375859599")' > $install_dir/mods/dedicated_server_mods_setup.lua 
+# ./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 +quit
+
+# ENABLING MOD
+# the IDs are suppose to go into a declare an indexed array first.
+# However array is it is shell dependent. And I fail to push new element into it(/bin/bash):
+#
+# modIDs=()
+#   ...
+#   then
+#       modIDs+=( "$modID" ) # does not work
+#   fi
+#   ...
+# echo "${modIDs[@]}" # value is not inserted
+#
+# so I stop bother
+cat ./cluster-content/Master/modoverrides.lua | while read line
+do
+    if [[ $line == '["workshop-'* ]]
+    then
+        # $line === ["workshop-378160973"] = { enabled = true }, 
+        modID=`echo "$line" | sed -e 's/\["workshop-//g' -e 's/"\].*//g'`
+        # $modID === 378160973
+        echo "ServerModSetup(\"$modID\")" >> $install_dir/mods/dedicated_server_mods_setup.lua 
+    fi
+done 
 
 check_for_file "$install_dir/bin"
 
