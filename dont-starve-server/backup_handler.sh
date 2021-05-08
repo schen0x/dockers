e8bc86c5cb74268cf61b05ddbb174b056e45ff2c
schen0x <schen.git@gmail.com>
2021-05-09 01:58:21 +0900

    io

--- dont-starve-server/backup_handler.sh
+++ dont-starve-server/backup_handler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # usage: sh ./backup_handler.sh $SERVER_IP
-HOST=$1
+HOST="root@$1"
 BACKUP_SCRIPT_FILE="./run_dedicated_server_backup.sh"
 echo "try to backup save files for HOST: $HOST"
 if ssh "$HOST" < $BACKUP_SCRIPT_FILE; then
--- dont-starve-server/restore_handler.sh
+++ dont-starve-server/restore_handler.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # shell dependent
 # usage: bash ./restore_handler.sh $SERVER_IP
-HOST=$1
+HOST="root@$1"
 echo ""
 read -p "restoring save files for HOST: $HOST, Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 BACKUP_FILE_NAME="cluster_content_backup.zip"
 TMP_FOLDER="/tmp"
-TARGET_PATH="~/dockers/dont-starve-server/cluster-content/"
+TARGET_FOLDER="~/dockers/dont-starve-server/cluster-content"
 if scp ./$BACKUP_FILE_NAME "$HOST:$TMP_FOLDER/$BACKUP_FILE_NAME"; then
-    ssh -t "$HOST" "cd ~/; tar -xvzf $TMP_FOLDER/$BACKUP_FILE_NAME -C $TARGET_PATH; cd $TARGET_PATH/../; docker-compose up"
+    ssh -t "$HOST" "cd ~/; tar -xvzf $TMP_FOLDER/$BACKUP_FILE_NAME -C $TARGET_FOLDER/; cd $TARGET_FOLDER/../; docker-compose up &"
+    ssh "$HOST" "cd $TARGET_FOLDER/../; nohup docker-compose up"
 fi
